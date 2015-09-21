07e5ae4ac69655ba2f4c801c106ec71c8c716b34
Dewar Tan <Dewar0019@gmail.com>
2015-09-21 18:29:11 -0400

    trying prof files

--- previousFiles/app.rb
+++ previousFiles/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require './config/environments' # database configuration
-require './models/model'
+require './config/environments' #database configuration
+require './models/model'        #Model class
 
 get '/' do
 	erb :index
@@ -9,7 +9,7 @@ end
 
 post '/submit' do
 	@model = Model.new(params[:model])
-	if @model.save
+	if @model.save #save method comes from active record
 		redirect '/models'
 	else
 		"Sorry, there was an error!"
